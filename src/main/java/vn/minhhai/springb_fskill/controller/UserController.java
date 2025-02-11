6b1898e808c545c37bd95ebc07a844bd5d0e7718
minhhai205 <158561042+minhhai205@users.noreply.github.com>
2025-02-11 14:46:39 +0700

    Validate @PathVariable UserController

--- src/main/java/vn/minhhai/springb_fskill/controller/UserController.java
+++ src/main/java/vn/minhhai/springb_fskill/controller/UserController.java
@@ -3,6 +3,7 @@ package vn.minhhai.springb_fskill.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
 import vn.minhhai.springb_fskill.dto.request.UserRequestDTO;
 import vn.minhhai.springb_fskill.dto.response.ResponseData;
 import vn.minhhai.springb_fskill.dto.response.ResponseError;
@@ -14,12 +15,14 @@ import org.springframework.web.bind.annotation.PutMapping;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
+@Validated
 public class UserController {
 
     @PostMapping("/user/create")
@@ -28,24 +31,28 @@ public class UserController {
     }
 
     @PutMapping("user/{id}")
-    public ResponseData<?> updateUser(@PathVariable int id, @RequestBody UserRequestDTO userDTO) {
+    public ResponseData<?> updateUser(@PathVariable @Min(value = 1, message = "userId must be greater than 0") int id,
+            @RequestBody UserRequestDTO userDTO) {
         return new ResponseData<>(HttpStatus.ACCEPTED.value(), "Updated successFully");
     }
 
     @PatchMapping("user/{id}") // required : tham số không bắt buộc
-    public ResponseData<?> changeUserStatus(@PathVariable int id, @RequestParam(required = false) boolean status) {
+    public ResponseData<?> changeUserStatus(
+            @PathVariable @Min(value = 1, message = "userId must be greater than 0") int id,
+            @RequestParam(required = false) boolean status) {
         // return new ResponseData<>(HttpStatus.ACCEPTED.value(), "User Status
         // changed");
         return new ResponseError(HttpStatus.BAD_REQUEST.value(), "Change Status failed");
     }
 
     @DeleteMapping("user/delete/{id}")
-    public ResponseData<?> deleteUser(@PathVariable int id) {
+    public ResponseData<?> deleteUser(@PathVariable @Min(value = 1, message = "userId must be greater than 0") int id) {
         return new ResponseData<>(HttpStatus.NO_CONTENT.value(), "User deleted");
     }
 
     @GetMapping("user/{id}")
-    public ResponseData<UserRequestDTO> getUser(@PathVariable int id) {
+    public ResponseData<UserRequestDTO> getUser(
+            @PathVariable @Min(value = 1, message = "userId must be greater than 0") int id) {
         return new ResponseData<>(HttpStatus.OK.value(), "User deleted",
                 new UserRequestDTO("Minh", "Hai", "admin@gmail.vn", "0123456789"));
     }
