eebe657e89158599e01e9a812e1f975b762d831d
minhhai205 <158561042+minhhai205@users.noreply.github.com>
2025-02-12 16:10:40 +0700

    Cấu Hình CORS Trong Spring Boot - Dùng WebMvcConfigurer - B_9

---
+++ src/main/java/vn/minhhai/springb_fskill/config/AppConfig.java
@@ -0,0 +1,19 @@
+package vn.minhhai.springb_fskill.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class AppConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowCredentials(true)
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+
+}
